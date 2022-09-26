af5910300b67ea2f58745663c813d1379a933f57
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2022-09-27 02:21:47 +0530

    Add files via upload

---
+++ PrintFunctionPart2.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// print() and println() function are alternative to Print() and Println() functions in fmt package
+	print("Trying to print the content to console without using fmt package \n")
+	println("Trying to print the content to console and moving the cursor to next line without using fmt package")
+}
