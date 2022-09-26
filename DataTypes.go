a19ffb18ac4d000e422dfcf5702ca46ca788517a
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2022-09-27 03:25:00 +0530

    Add files via upload

---
+++ DataTypes.go
@@ -0,0 +1,45 @@
+/*
+* Data Type specifies the type of data variable can hold
+* It also specifies, the size of memory which needs to be allocated for the variable
+
+A> Data Types are divided into 4 categories
+---------------------------------------------
+1. Basic Type
+2. Aggregate Type
+3. Reference Type
+4. Interface Type
+
+	a> Basic data types are further categorised into 3 types
+	--------------------------------------------------------
+	1. Numbers
+	2. Booleans
+	3. Strings
+
+		Numbers
+		--------
+		Numbers are again categorised into 3 sub categories
+ 		1. integers
+ 		2. floats-point numbers
+ 		3. complex numbers
+
+			1.Integers
+			----------
+			int - 4/8 bytes, depends up on the processor
+			uint/uintptr - unsigned int
+			int8 - 8-bit signed integer
+			int16 - 16 bit signed integer
+			int32/rune - 32 bit signed integer
+			int64 - 64 bit signed integer
+			uint8/byte - 8 bit unsigned integer
+			unit16 - 16 bit unsigned integer
+			unit32 - 32 bit unsigned integer
+			unit64 - 64 bit unsigned integer
+			uintptr - For representing pointer variable
+
+
+*/
+package main
+
+func main() {
+
+}
